b653f3e3e9a5e5e304d27419a69c5c623b8e8441
FranRM15 <franjrm15@gmail.com>
2022-05-02 22:38:41 -0500

    [ADDED] New line

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from asyncore import write
 import sys
-str = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+str = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
 
 write(2, str, len(str))
 exit(1)
