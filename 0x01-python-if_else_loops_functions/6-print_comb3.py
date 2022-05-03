c52cf5dc69b3ea49c95b436f21171692008b9ffc
Francisco Ramírez <98773774+FranRM15@users.noreply.github.com>
2022-05-03 17:56:15 -0500

    [DELETED] file 6 by failed commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-for a in range(0, 10):
-    for b in range(0, 10):
-        if a != b and a < b:
-            print(f"{a}{b}", end="")
-            if a != 8:
-                print(", ", end="")
-print()
