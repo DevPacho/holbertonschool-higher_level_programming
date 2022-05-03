61bbeacda12ba05c28e92c9bcf2f784dc6c36063
FranRM15 <franjrm15@gmail.com>
2022-05-03 14:03:58 -0500

    [FIXED] Output with a negative number '* -1'

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastd = abs(number) % 10
-if lastd % 10 > 5:
-    print(f"Last digit of {number} is {lastd} and is greater than 5")
-if lastd % 10 == 0:
-    print(f"Last digit of {number} is {lastd} and is 0")
-if lastd % 10 < 6 and number % 10 != 0:
-    print(f"Last digit of {number} is {lastd} and is less than 6 and not 0")
+if number < 0:
+    savenum = number % 10 * -1
+if number >= 0:
+    savenum = number % 10
+
+if savenum % 10 > 5:
+    print(f"Last digit of {number} is {savenum} and is greater than 5")
+if savenum % 10 == 0:
+    print(f"Last digit of {number} is {savenum} and is 0")
+if savenum % 10 < 6 and number % 10 != 0:
+    print(f"Last digit of {number} is {savenum} and is less than 6 and not 0")
