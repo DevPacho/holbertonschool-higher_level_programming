4bbcf521aee375b4300948a071464a3a506b3c17
FranRM15 <franjrm15@gmail.com>
2022-05-03 22:29:38 -0500

    [FIXED] Order of the instructions, first negative number case and then, module of the number

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    number = number % 10
-
     if number < 0:
-        number = (number * -1)
+        number = number * -1
+    number = number % 10
     print(number, end="")
     return(number)
