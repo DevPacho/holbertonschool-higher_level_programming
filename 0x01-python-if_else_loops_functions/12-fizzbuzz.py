188546e65266119640a77119c401e35e5dff1a8e
FranRM15 <franjrm15@gmail.com>
2022-05-03 23:37:38 -0500

    [ADDED] print FizzBuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 for a in range(1, 101):
     if a % 3 == 0 or a % 5 == 0:
+        print("FizzBuzz", end=" ")
         if a % 3 == 0:
-            print("Fizz", end="")
+            print("Fizz", end=" ")
             if a % 5 == 0:
-                print("Buzz", end="")
+                print("Buzz", end=" ")
     else:
-        print(f"{a}", end="")
-    if a != 100:
-        print(" ")
+        print(f"{a}", end=" ")
