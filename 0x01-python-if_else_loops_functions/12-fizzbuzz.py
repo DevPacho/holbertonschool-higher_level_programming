ae4be3e43b9e763d44a897a4c0970eedf9c895ab
FranRM15 <franjrm15@gmail.com>
2022-05-03 23:39:35 -0500

    [FIXED] Indentation order

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,9 +2,9 @@
 for a in range(1, 101):
     if a % 3 == 0 or a % 5 == 0:
         print("FizzBuzz", end=" ")
-        if a % 3 == 0:
-            print("Fizz", end=" ")
-            if a % 5 == 0:
-                print("Buzz", end=" ")
+    if a % 3 == 0:
+        print("Fizz", end=" ")
+    if a % 5 == 0:
+        print("Buzz", end=" ")
     else:
         print(f"{a}", end=" ")
