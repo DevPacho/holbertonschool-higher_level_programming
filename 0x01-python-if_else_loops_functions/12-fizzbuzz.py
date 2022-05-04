79fa5e9028b17d6d704e1453e86b9d6cd0978397
FranRM15 <franjrm15@gmail.com>
2022-05-03 23:26:48 -0500

    12. Fizz Buzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for a in range(1, 101):
+    if a % 3 == 0 or a % 5 == 0:
+        if a % 3 == 0:
+            print("Fizz", end="")
+            if a % 5 == 0:
+                print("Buzz", end="")
+    else:
+        print(f"{a}", end="")
+print()
