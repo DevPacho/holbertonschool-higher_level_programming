65c552d23e95190345b67a567a5f1578e45f5317
FranRM15 <franjrm15@gmail.com>
2022-05-04 21:41:27 -0500

    [FIXED] Output by missing validations, second if return and bad logic

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -10,6 +10,7 @@ listint_t *insert_node(listint_t **head, int number)
     listint_t *newnode, *tosave = *head;
 
     newnode = malloc(sizeof(listint_t));
+    newnode->n = number;
 
     if (!newnode)
         return (NULL);
@@ -18,14 +19,21 @@ listint_t *insert_node(listint_t **head, int number)
     {
         newnode->next = tosave;
         tosave = newnode;
+        return (newnode);
     }
     else
     {
-        while (tosave->next != NULL && tosave->next->n < newnode->n)
+        while (tosave->next && tosave != NULL && tosave->n < newnode->n)
             tosave = tosave->next;
         newnode->next = tosave->next;
-        tosave->next = newnode->next;
-    }
+        tosave->next = newnode;
 
-    return (newnode);
+        return (newnode);
+    }
 }
+
+/**To insert in the middle, we need my data to be less than the previous one in the list 
+ * and greater than the next one, it is not enough to validate only one option, 
+ * take into account checkers when inserting at the beginning or end, 
+ * besides saving the data already existing in the list in a temporary pointer so that 
+ * they are not lost when entering the new ones. */
