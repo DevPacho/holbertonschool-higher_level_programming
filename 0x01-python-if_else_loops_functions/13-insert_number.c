a04a27d0c8a1f767e92f07c85d6fd06a5112acac
FranRM15 <franjrm15@gmail.com>
2022-05-04 21:57:52 -0500

    [FIXED] I forgot that in C indentation is not with spaces.

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,39 +1,45 @@
 #include "lists.h"
 
 /**
- * @brief 
- * 
+ * insert_node - function that inserts a number into a sorted
+ * singly linked list.
+ * @head: head of the list.
+ * @number: number to insert.
+ * Return: the address of the new node, or NULL if it failed.
  */
 
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *newnode, *tosave = *head;
+	listint_t *newnode, *tosave = *head;
 
-    newnode = malloc(sizeof(listint_t));
-    newnode->n = number;
+	newnode = malloc(sizeof(listint_t));
+	newnode->n = number;
 
-    if (!newnode)
-        return (NULL);
+	if (!newnode)
+		return (NULL);
 
-    if (!tosave || tosave->n >= newnode->n)
-    {
-        newnode->next = tosave;
-        tosave = newnode;
-        return (newnode);
-    }
-    else
-    {
-        while (tosave->next && tosave != NULL && tosave->n < newnode->n)
-            tosave = tosave->next;
-        newnode->next = tosave->next;
-        tosave->next = newnode;
+	if (!tosave || tosave->n >= newnode->n)
+	{
+		newnode->next = tosave;
+		tosave = newnode;
+		return (newnode);
+	}
+	else
+	{
+		while (tosave->next && tosave != NULL && tosave->n < newnode->n)
+			tosave = tosave->next;
+		newnode->next = tosave->next;
+		tosave->next = newnode;
 
-        return (newnode);
-    }
+	return (newnode);
+	}
 }
 
-/**To insert in the middle, we need my data to be less than the previous one in the list 
- * and greater than the next one, it is not enough to validate only one option, 
- * take into account checkers when inserting at the beginning or end, 
- * besides saving the data already existing in the list in a temporary pointer so that 
- * they are not lost when entering the new ones. */
+/**
+ * insert_node - To insert in the middle, we need my data to be less than
+ * the previous one in the list and greater than the next one, it is not
+ * enough to validate only one option, take into account checkers when
+ * inserting at the beginning or end, besides saving the data already
+ * existing in the list in a temporary pointer so that they are not lost
+ * when entering the new ones.
+ */
