f9050f5dd7bd643258712296dfd6e3b2711e6664
FranRM15 <franjrm15@gmail.com>
2022-05-05 22:02:08 -0500

    [FIXED] pycodestyle by whitespaces in lines

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,16 +9,16 @@ if __name__ == "__main__":
     if argv != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    
+
     if argv[2] != "+-*/":
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    
+
     if argv[2] == "+":
         print(f"{a} + {b} = {add(a, b)}")
     if argv[2] == "-":
         print(f"{a} - {b} = {sub(a, b)}")
-    if argv[2] == "\*":
+    if argv[2] == "*":
         print(f"{a} * {b} = {mul(a, b)}")
     if argv[2] == "/":
         print(f"{a} / {b} = {div(a, b)}")
