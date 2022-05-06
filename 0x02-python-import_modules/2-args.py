5c1c5d3b9958d9d8df2f1e07ccfb83df85d382f8
FranRM15 <franjrm15@gmail.com>
2022-05-05 19:15:02 -0500

    [FIXED] Output by iterator case == 1 and 'argument', not 'arguments'

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,12 @@
 from sys import argv
 
 if __name__ == "__main__":
-    if len(argv) == 1:
+    if len(argv) - 1 == 0:
         print(f"0 arguments.")
 
     elif len(argv) == 2:
-        print(f"{len(argv)} argument:")
-        print(f"{len(argv)}: " + argv[1])
+        print(f"{len(argv) - 1} argument:")
+        print(f"{len(argv) - 1}: " + argv[1])
 
     elif len(argv) > 2:
         print(f"{len(argv) - 1} arguments:")
