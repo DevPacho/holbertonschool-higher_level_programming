87e4b2910b1b8bd2551fbf8c421e1df5553d7a90
DevPacho <frankjose2004@gmail.com>
2022-05-09 22:25:40 -0500

    [FIXED] output

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     cpy = my_list.copy()
+    b = 0
     for a in cpy:
         if a % 2 == 0:
-            cpy[a] = True
+            cpy[b] = True
         else:
-            cpy[a] = False
-        a += 1
+            cpy[b] = False
+        b += 1
     return cpy
