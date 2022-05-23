466b41c483f4d5f086f778e6dea0398d1227c426
DevPacho <frankjose2004@gmail.com>
2022-05-23 14:05:15 -0500

    [CHANGED] loop order and print format

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     to_increase = 0
-    numbers = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
-    try:
-        for traverse in range(x):
-            if traverse in numbers:
-                print(my_list[traverse], end="")
-                to_increase += 1
 
-    except Exception:
-        pass
+    for traverse in range(x):
+        try:
+            print("{:d}".format(my_list[traverse]), end="")
+            to_increase += 1
+
+        except Exception:
+            pass
     print()
     return to_increase
