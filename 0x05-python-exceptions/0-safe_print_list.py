88663d54923d2df8d09ceb3dd5beddbcaed692f7
DevPacho <frankjose2004@gmail.com>
2022-05-23 12:00:04 -0500

    [FIXED] pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for traverse in range(x):
             print(my_list[traverse], end="")
             to_increase += 1
-    
+
     except IndexError:
         pass
     print()
