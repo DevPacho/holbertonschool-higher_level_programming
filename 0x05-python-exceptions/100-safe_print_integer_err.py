23cdee49e99caafbd9f90900cc03164556eeae76
DevPacho <frankjose2004@gmail.com>
2022-05-23 23:03:32 -0500

    [FIXED] no validations for the value

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,9 +3,8 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        if value < 0 or value >= 0:
-            print("{:d}".format(value))
-            return True
+        print("{:d}".format(value))
+        return True
     except Exception as ex:
         sys.stderr.write("Exception: {}\n".format(ex))
         return False
