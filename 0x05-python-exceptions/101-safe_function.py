1d4cf5785ed9dd04e0c476a709c35766e1a2520c
DevPacho <frankjose2004@gmail.com>
2022-05-23 23:26:17 -0500

    Trying with alias in Exception

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,6 +5,6 @@ import sys
 def safe_function(fct, *args):
     try:
         return fct(*args)
-    except Exception:
-        sys.stderr.write("Exception: {}\n".format(Exception))
+    except Exception as ex:
+        sys.stderr.write("Exception: {}\n".format(ex))
         return None
