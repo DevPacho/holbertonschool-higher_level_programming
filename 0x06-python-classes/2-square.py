777d9ed06f198a95e8806b65df2cd8e1b8ecd93a
DevPacho <frankjose2004@gmail.com>
2022-05-24 19:59:31 -0500

    [FIXED] Python Docstrings

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,8 +4,9 @@
 class Square:
     """The value is assigned for the size of the square"""
     def __init__(self, size = 0):
-        """Private instance attribute 'size' with instantiation 'size'"""
+        """'size' is an integer?"""
         if type(size) is int:
+            """Private instance attribute 'size' with instantiation 'size'"""
             self.__size = size
         else:
             raise TypeError("size must be an integer")
