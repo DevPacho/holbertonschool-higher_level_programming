f137b734bc782281845d79d9d0fbeb03ceddea4e
DevPacho <frankjose2004@gmail.com>
2022-05-31 19:21:31 -0500

    [CHANGED] No private attributes in __init__ method

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """Instance method called when a new object is created"""
     def __init__(self, width=0, height=0):
         """Attributes"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
