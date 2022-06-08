54c726470795c4721417c1a5a2b5397df2df3926
DevPacho <frankjose2004@gmail.com>
2022-06-08 13:47:24 -0500

    [FIXED] functions documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Function that reads a text file (UTF8) and prints it to stdout"""
+
+
 def read_file(filename=""):
+    """Reads and prints"""
+
     with open(filename, "r") as f:
         content = f.read()
     print(content)
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""function that writes a string to a text file (UTF8)
+    and returns the number of characters written"""
+
+
 def write_file(filename="", text=""):
+    """Writes and returns the chars written quantity"""
     with open(filename, "w") as f:
         f.write(text)
 
