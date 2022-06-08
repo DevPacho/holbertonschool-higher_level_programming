23acc74a021394901d3213e1483dd8504742c584
DevPacho <frankjose2004@gmail.com>
2022-06-08 17:20:12 -0500

    [ADDED] encoding

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,6 @@
 def read_file(filename=""):
     """Reads and prints"""
 
-    with open(filename, "r") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         content = f.read()
     print(content)
