e9de67b026dff4af79266ebdddd63543e41ef717
DevPacho <frankjose2004@gmail.com>
2022-06-08 22:20:53 -0500

    10. Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Class 'Student'"""
+
+
+class Student:
+
+    """Constructor method"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation"""
+        dictionary = {}
+
+        if attrs is None:
+            return self.__dict__
+
+        for key_inside, value_inside in self.__dict__.items():
+            if key_inside in attrs:
+                dictionary[key_inside] = value_inside
+        return dictionary
