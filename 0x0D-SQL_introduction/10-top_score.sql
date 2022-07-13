d96d0ac0e6e52710a8f38a20a0dd69f3f53c4391
DevPacho <frankjose2004@gmail.com>
2022-07-13 15:54:35 -0500

    [FIXED] output with score and name fields

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
 -- List some records ordered in a specific way.
 -- Script that lists all records of the table 'second_table', but
--- the records should be ordered by score (top first).
+-- this records should be ordered by score (top first).
 
 SELECT score, name FROM second_table ORDER BY score DESC;
