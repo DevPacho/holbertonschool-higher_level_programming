78fe8ff207159522faaefe2049999e1737328a75
DevPacho <frankjose2004@gmail.com>
2022-07-13 15:52:42 -0500

    10. List by best

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -2,4 +2,4 @@
 -- Script that lists all records of the table 'second_table', but
 -- the records should be ordered by score (top first).
 
-SELECT score FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
