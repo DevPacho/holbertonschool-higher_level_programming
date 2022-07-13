415f24f07cbcd8fe8dbbb36e6fe60cb510c7c802
DevPacho <frankjose2004@gmail.com>
2022-07-13 18:16:11 -0500

    15. Number by score

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,8 @@
+-- Lists records with the same value.
+-- Script that lists the number of records with the same score
+-- in the table 'second_table' of the database 'hbtn_0c_0'
+
+SELECT score,
+COUNT(*) "number"
+FROM second_table
+GROUP BY score ORDER BY number DESC;
