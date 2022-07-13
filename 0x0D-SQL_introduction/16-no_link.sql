8896999b312cfd5475b11960955c007dfaeea0c0
DevPacho <frankjose2004@gmail.com>
2022-07-13 18:31:21 -0500

    16. Say my name

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,8 @@
+-- List records with more conditions.
+--  script that lists all records of the table
+-- 'second_table' of the database 'hbtn_0c_0'.
+
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
