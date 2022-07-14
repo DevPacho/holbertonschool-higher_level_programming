a7004989157ec672aaf68eded683cf1757cffca5
DevPacho <frankjose2004@gmail.com>
2022-07-14 17:45:23 -0500

    4. ID can't be null

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,7 @@
+-- Craeting another table.
+-- Script that creates the table 'id_not_null' with some requeriments.
+
+CREATE TABLE IF NOT EXISTS id_not_null (
+	id INT DEFAULT 1,
+	name VARCHAR(256)
+);
