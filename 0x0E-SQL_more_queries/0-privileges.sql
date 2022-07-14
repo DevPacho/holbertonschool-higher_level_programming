267955251a84255e4a2beded55f30910c4bce2be
DevPacho <frankjose2004@gmail.com>
2022-07-14 15:15:39 -0500

    [CHANGED] double quotation marks for single quotes

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -2,5 +2,5 @@
 -- Script that lists all privileges of the MySQL users 'user_0d_1'
 -- and 'user_0d_2' on your server (in localhost).
 
-SH0W GRANTS FOR "user_0d_1"@"localhost";
-SH0W GRANTS FOR "user_0d_2"@"localhost";
+SH0W GRANTS FOR 'user_0d_1'@'localhost';
+SH0W GRANTS FOR 'user_0d_2'@'localhost';
