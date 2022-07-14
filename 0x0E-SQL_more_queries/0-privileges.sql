f95e0c7976ae772570fb56bc3a6366b5e9c49212
DevPacho <frankjose2004@gmail.com>
2022-07-14 15:26:56 -0500

    Trying with WSL directly

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,6 +1,6 @@
--- Lists privileges.
+-- Lists users privileges.
 -- Script that lists all privileges of the MySQL users 'user_0d_1'
 -- and 'user_0d_2' on your server (in localhost).
 
-SH0W GRANTS FOR 'user_0d_1'@'localhost';
-SH0W GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
