8ad6c09e7a022290798d7ab5443b9ae85f7f3853
DevPacho <frankjose2004@gmail.com>
2022-07-14 19:07:34 -0500

    [CHANGED] order swapped

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- Lists some specifics values that can be found in the database 'hbtn_0d_usa'.
 
-SELECT cities,
+SELECT states,
 WHERE name = "California"
-FROM states
+FROM cities
 ORDER BY cities.id ASC;
