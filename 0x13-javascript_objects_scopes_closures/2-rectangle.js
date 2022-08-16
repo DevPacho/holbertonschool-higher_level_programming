6d6b440c8c6b69b78f3f93f0fbcd2703cdfb0a69
DevPacho <frankjose2004@gmail.com>
2022-08-16 14:53:05 -0500

    [FIXED] a more simple validation

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor (w, h) {
-      if (w === 0 || h ===0 || !Number(w) || !Number(h)) {
-        this.w = {}
-        this.h = {}
-
-      } else {
-        this.width = w;
-        this.height = h;
-      }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
-  
-  module.exports = Rectangle;
+}
+
+module.exports = Rectangle;
