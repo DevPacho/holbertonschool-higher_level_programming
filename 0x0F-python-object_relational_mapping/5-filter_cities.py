75ed1df37bf03d82a6a8eb460f29a2ce98bef776
DevPacho <frankjose2004@gmail.com>
2022-08-31 01:25:34 -0500

    [FIXED

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -2,6 +2,7 @@
 """ Script that takes in the name of a state as an argument and
 lists all 'cities' of that state, using the database 'hbtn_0e_4_usa' """
 
+from ntpath import join
 import MySQLdb
 from sys import argv
 
@@ -14,11 +15,12 @@ if __name__ == "__main__":
                            db=argv[3])
     cur = conn.cursor()
     cur.execute("SELECT cities.name FROM cities \
-        JOIN states on states.id = cities.state_id \
+        JOIN states ON states.id = cities.state_id \
         WHERE states.name = %s ORDER BY cities.id", (
         argv[4],))
     query_rows = cur.fetchall()
-    for row in query_rows:
-        print(', '.join(city[0] for city in row))
+    formatted = ", ".join(row[0] for row in query_rows)
+    print(formatted)
+
     cur.close()
     conn.close()
