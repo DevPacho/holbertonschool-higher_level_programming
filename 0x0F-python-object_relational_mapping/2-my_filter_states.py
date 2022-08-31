d8cdce45ad203aa8d55775b19564f6155be7f43d
DevPacho <frankjose2004@gmail.com>
2022-08-30 19:16:41 -0500

    [FIXED] results must be sorted in ascending order by states.id

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,9 @@ if __name__ == "__main__":
                            passwd=argv[2],
                            db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}'".format(argv[4]))
+    query = "SELECT * FROM states WHERE name = '{}' \
+        ORDER BY states.id".format(argv[4])
+    cur.execute()
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
