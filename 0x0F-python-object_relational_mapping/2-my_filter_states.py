704265d0edd5a4d8a36c81156645b7e7fcffae1b
DevPacho <frankjose2004@gmail.com>
2022-08-30 19:46:26 -0500

    [ADDED] tuple position validation

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" script that takes in an argument and displays all values
+""" Script that takes in an argument and displays all values
 in the 'states' table of 'hbtn_0e_0_usa' where 'name' matches the argument. """
 
 import MySQLdb
@@ -17,6 +17,7 @@ if __name__ == "__main__":
         ORDER BY states.id".format(argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
-        print(row)
+        if row[1] == argv[4]:
+            print(row)
     cur.close()
     conn.close()
