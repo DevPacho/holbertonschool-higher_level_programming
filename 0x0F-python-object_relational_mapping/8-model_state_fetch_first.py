cec7870919c6043456ab228dc6c650603ab48057
DevPacho <frankjose2004@gmail.com>
2022-09-01 00:01:53 -0500

    [CHANGED] empty table validation

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     session_instance = Session_maker()
 
     table_in_list = session_instance.query(State).order_by(State.id).first()
-    if (State is None):
+    if (table_in_list is None):
         print("Nothing")
     else:
         print(f"{table_in_list.id}: {table_in_list.name}")
