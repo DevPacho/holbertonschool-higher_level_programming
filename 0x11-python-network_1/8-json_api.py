7c64b6c6ce5d4a0e0ad859e36a07a61abfe5f0fb
DevPacho <frankjose2004@gmail.com>
2022-09-04 00:09:58 -0500

    Everything works except when no argument is received

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Script that takes in a letter and sends a POST request
+to 'http://0.0.0.0:5000/search_user' with the letter as a parameter. """
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    letter = argv[1]
+    url = "http://0.0.0.0:5000/search_user"
+
+    if len(argv) > 1:
+        send_letter = requests.post(url, data={"q": letter})
+    else:
+        send_letter = requests.post(url, data={"q": ""})
+
+    try:
+        json_format = send_letter.json()
+
+        if json_format:
+            get_id = json_format.get('id')
+            get_name = json_format.get('name')
+            print("[{}] {}".format(get_id, get_name))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
